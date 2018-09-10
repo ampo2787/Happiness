9731a2c3d95c6a05c522ff8429fc253a3e4228f8
Your Name <you@your-domain.com>
2018-09-10 16:10:59 +0900

    Initial Commit

---
+++ Happiness_02_201402356/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Happiness_02_201402356
+//
+//  Created by JihoonPark on 2018. 9. 10..
+//  Copyright © 2018년 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Happiness_02_201402356/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Happiness_02_201402356
+//
+//  Created by JihoonPark on 2018. 9. 10..
+//  Copyright © 2018년 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
